7e2283cd5772957fbbb02185c8afb750940ff4fb
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-16 22:16:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const guildID = "546385232698408970";
-var memberCount = client.guilds.get(guildID).members.size()
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -15,22 +13,16 @@ client.on('message', message => {
   description: "**Site:** https://redeslash.com\n**Twitter:** @SlashNetworkOFC\n**Criador:** <@414387339629035520>"
 }});
   	}
-    if (message.content === '+entrada') {
-        message.channel.send({embed: {
-  color: 0x4bf442,
-  description: ":green_heart: Servidores SlashMC :green_heart:\n\n:inbox_tray: HugoBessaPT entrou no servidor!\nAtualmente somos" + memberCount "membros no servidor!"
-}});
-    }
     if (message.content === 'O que fazer com um dominio?') {
     	message.channel.send('Faça igual ao Azalim, pegue nele e enfie no cu! 😆😆');
     }
 });
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('546432785666015348').send('**' + member.user.username + '**, juntou-se ao servidor que merda mano!'); 
+    member.guild.channels.get('546432785666015348').send(':green_heart: Servidores SlashMC :green_heart:\n\n:inbox_tray: **' + member.user.username + '**, entrou no servidor!\nAtualmente somos **?** membros no servidor!'); 
 });
 
 client.on('guildMemberRemove', member => {
-    member.guild.channels.get('546432785666015348').send('**' + member.user.username + '**, aleluia o fdp saiu kk');
+    member.guild.channels.get('546432785666015348').send(':green_heart: Servidores SlashMC :green_heart:\n\n:outbox_tray: **' + member.user.username + '**, saiu do servidor!\nAtualmente somos **?** membros no servidor!');
 });
 
 // THIS  MUST  BE  THIS  WAY
