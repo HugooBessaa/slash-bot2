815514d1f6f0ae36ecc77d3699e62e485d21dc41
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-17 15:59:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame("jogar.redeslash.com");
+    client.user.setGame('Hello!');
 });
 
 client.on('message', message => {
