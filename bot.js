4582b44516300ae2f45e2cbdea6eebfb025f9ff9
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-17 11:14:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('guildMemberAdd', member => {
         g.members.forEach((member) => {
             count++;
         });
-    member.guild.channels.get('546432785666015348').send(':green_heart: Servidores SlashMC :green_heart:\n\n:inbox_tray: **' + member.user.username + '**, entrou no servidor!\nAtualmente somos **'count'** membros no servidor!'); 
+    member.guild.channels.get('546432785666015348').send(':green_heart: Servidores SlashMC :green_heart:\n\n:inbox_tray: **' + member.user.username + '**, entrou no servidor!\nAtualmente somos **' + count + '** membros no servidor!'); 
 });
 
 client.on('guildMemberRemove', member => {
