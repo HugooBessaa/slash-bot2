ad9170d24580bc36a6bad1fee7f7b6fa4df9186a
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-17 15:15:55 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const FS = require('fs');
+
+function changing_status() {
+  let status = [`1`, `2`, `3`]
+  let random = status[Math.floor(Math.random() * status.length)]
+  client.user.setActivity(random)
+}
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity('jogar.redeslash.com', { type: 'PLAYING' });
+    setInterval(changing_status,10000);
 });
 
 client.on('message', message => {
