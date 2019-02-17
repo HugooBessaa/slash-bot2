49932110b71ff2f84a4e2d2419284a74e91779ed
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-17 17:09:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ client.on('message', async (message) => {
 	let args = message.content.split(" ").slice(1);
     
     if(command === "sugestão") {
+	    if(args[0]]) return message.channel.send("Utilize +sugestão <sua sugestão>");
 		message.delete()
         const embed = new Discord.RichEmbed()
 		.setColor(0x4bf442)
