ebc45b0c37c407b47ffed6e1ee66e888997f601a
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-23 12:39:51 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,10 +43,11 @@ client.on('message', async (message) => {
     if(command === "testarentrada") {
         const embed = new Discord.RichEmbed()
 		.setColor(0x4bf442)
+	        .setTitle(":green_heart: Servidores SlashMC :green_heart:")
 		.setDescription(':green_heart: Servidores SlashMC :green_heart: \n\n:inbox_tray: **HugoBessaPT**, entrou no servidor!\n<:slashontop:546070971501510666> Seja bem vindo ao discord oficial da SlashMC!\nAconselhamos a ler as <#546049920365232148> para não ser punido!');
 	        embed.addField(":video_game: IP:", "jogar.redeslash.com")
 	        embed.addField(":shopping_cart: Loja:", "[Clique aqui](https://redeslash.com)")
-	        embed.addField(":bird: Twitter:", "[@SlashNetworkOFC](https://twitter.com/slashnetworkof)")
+	        embed.addField(":bird: Twitter:", "[@SlashNetworkOFC](https://twitter.com/slashnetworkofc)")
 	        
 		client.channels.get('546686267874869260').send({embed})
            message.channel.send({embed});
