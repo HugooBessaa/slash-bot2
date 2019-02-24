101f4dfd601b3b292e515bd806092c365e1daebc
Hugo Bessa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-24 11:00:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ if(!message.member.roles.some(r=>["🚧| Master", "🚧| Gerente", "🚧| Admini
 client.on('guildMemberAdd', member => {
         const embed = new Discord.RichEmbed()
 		.setColor(0x4bf442)
-		.setDescription(':green_heart: **Servidores SlashMC** :green_heart:\n\n:inbox_tray: **'+ member.user.username +'**, entrou no servidor!\n<:slashontop:546070971501510666> eja bem vindo ao discord oficial da SlashMC!\nAconselhamos a ler as <#546049920365232148> para não ser punido!');
+		.setDescription(':green_heart: **Servidores SlashMC** :green_heart:\n\n:inbox_tray: **'+ member.user.username +'**, entrou no servidor!\n<:slashontop:546070971501510666> Seja bem vindo ao discord oficial da SlashMC!\nAconselhamos a ler as <#546049920365232148> para não ser punido!');
 	    	embed.setThumbnail(member.user.avatarURL)
 	        embed.addField(":shopping_cart: Loja:", "[Clique aqui](https://redeslash.com)", true)
 	        embed.addField(":bird: Twitter:", "[@SlashNetworkOFC](https://twitter.com/slashnetworkofc)", true)
