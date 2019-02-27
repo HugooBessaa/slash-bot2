768c60426d28e08dc11a1d768d4978129e112c64
Hugo Bessa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-27 22:59:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,7 +108,7 @@ if(!message.member.roles.some(r=>["🚧| Master", "🚧| Gerente", "🚧| Admini
 });
 
 client.on("messageReactionAdd", (reaction, users) =>{
- if (reaction.message.channel === reaction.message.guild.channels.find('name', '🔏│captcha') && reaction.message.id === '549191117891960835' && reaction.emoji.name === '💚'){
+ if (reaction.message.channel === reaction.message.guild.channels.find('name', '🔏│captcha') && reaction.message.id === '549191117891960835' && reaction.emoji.id === '546684879153397779'){
 	users.addRole(users.guild.roles.find("id", '546066964569784320'));
         users.removeRole(users.guild.roles.find("id", '548943092481392641'));
  }
