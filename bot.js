a021a1e60e79e80ca006c1723bc78c72ca51fe52
Hugo Bessa <39413828+HugooBessaa@users.noreply.github.com>
2019-03-02 11:55:35 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,10 +87,11 @@ if(!message.member.roles.some(r=>["🚧| Master", "🚧| Gerente", "🚧| Admini
       .catch(error => message.reply(`Desculpe ${message.author} não foi possivel kickar pelo seguinte erro: ${error}`));
         const embed = new Discord.RichEmbed()
 		.setColor(0xce1e00)
-	        .setAuthor('Punimentos SlashMC | Kick ', message.author.displayAvatarURL)
+	        .setAuthor('Punimentos SlashMC | Kick ', client.user.avatarURL)
 		.setDescription(`:no_entry_sign: **<@${member.user.id}>** foi kickado do servidor!\n:warning: Motivo: ${reason}\nAconselhamos a ler as <#546049920365232148> para não ser punido!`)
-	       .setThumbnail(member.user.displayAvatarURL)
-	       .setFooter(`Autor do banimento: ${message.author.tag}`, message.author.displayAvatarURL);
+	        .setThumbnail(member.user.displayAvatarURL)
+	        .setFooter(`Autor do banimento: <@${message.author.id}>`, message.author.displayAvatarURL)
+	        .setTimestamp();
            client.channels.get('546049103382052866').send({embed})
   }
 	
