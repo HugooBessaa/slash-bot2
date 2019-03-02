19d24abdb561e588bf23338c334056eb8074505e
Hugo Bessa <39413828+HugooBessaa@users.noreply.github.com>
2019-03-02 13:49:13 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,7 +159,7 @@ client.on('guildMemberRemove', member => {
         const embed = new Discord.RichEmbed()
 		.setColor(0xce1e00)
 		.setDescription(':green_heart: **Servidores SlashMC** :green_heart:\n\n:outbox_tray: **'+ member.user.username +'**, saiu do servidor!\n<:slashontop:546070971501510666> Infelizmente perdemos um membro!\nAconselhamos a ler as <#546049920365232148> para não ser punido!');
-	    	embed.setThumbnail(member.user.avatarURL)
+	    	embed.setThumbnail(member.user.displayAvatarURL)
 	        embed.addField(":shopping_cart: Loja:", "[Clique aqui](https://redeslash.com)", true)
 	        embed.addField(":bird: Twitter:", "[@SlashNetworkOFC](https://twitter.com/slashnetworkofc)", true)
 	   member.guild.channels.get('546048881033609216').send({embed});
