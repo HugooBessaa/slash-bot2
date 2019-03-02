dac82d38d8f4312bf8fa4eac633a897b976ccf38
Hugo Bessa <39413828+HugooBessaa@users.noreply.github.com>
2019-03-02 11:11:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,7 +144,7 @@ client.on('guildMemberAdd', member => {
         const embed = new Discord.RichEmbed()
 		.setColor(0x4bf442)
 		.setDescription(':green_heart: **Servidores SlashMC** :green_heart:\n\n:inbox_tray: **'+ member.user.username +'**, entrou no servidor!\n<:slashontop:546070971501510666> Seja bem vindo ao discord oficial da SlashMC!\nAconselhamos a ler as <#546049920365232148> para não ser punido!');
-	    	embed.setThumbnail(member.user.avatarURL)
+	    	embed.setThumbnail(member.user.displayAvatarURL)
 	        embed.addField(":shopping_cart: Loja:", "[Clique aqui](https://redeslash.com)", true)
 	        embed.addField(":bird: Twitter:", "[@SlashNetworkOFC](https://twitter.com/slashnetworkofc)", true)
 	   member.guild.channels.get('546048881033609216').send({embed});
